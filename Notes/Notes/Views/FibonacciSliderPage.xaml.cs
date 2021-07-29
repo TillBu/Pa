8c0d78f7b1892a0b7e2c620d61bbe0c211300553
h3ko Dev <dev@h3ko.de>
2021-07-29 15:22:47 +0200

    "13_Fibonacci" Fibonacci Number Label working

--- Notes/Notes/Views/FibonacciSliderPage.xaml.cs
+++ Notes/Notes/Views/FibonacciSliderPage.xaml.cs
@@ -28,8 +28,38 @@ namespace Notes.Views
             int Stage = (int)Math.Round(FibonacciSlider.Value);
 
             labelValue.Text = Stage.ToString();
-           
-            
+
+            int Fibonacci = CalculateLabel(Stage);
+
+            labelFibonacci.Text = Fibonacci.ToString();
+
             }
+
+        private int CalculateLabel(int Stage)
+        {
+            if (Stage == 12)
+            {
+                return 100;
+            }
+            else
+            {
+               return Fibonacci(Stage);
+
+            }
+        }
+            public static int Fibonacci(int n)
+            {
+                int a = 0;
+                int b = 1;
+                // In N steps, compute Fibonacci sequence iteratively.
+                for (int i = 0; i < n; i++)
+                {
+                    int temp = a;
+                    a = b;
+                    b = temp + b;
+                }
+                return a;
+            }
+        }
     }
-}
+
