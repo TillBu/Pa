a380772f61d0400d0b8066d22a86ecdc444a5dbe
h3ko Dev <dev@h3ko.de>
2021-07-29 11:37:39 +0200

    "7_Hello_Xamarin_Forms" Added First Tutorial Functions

--- Notes/Notes/AppShell.xaml.cs
+++ Notes/Notes/AppShell.xaml.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using Notes.ViewModels;
-using Notes.Views;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace Notes
 {
-    public partial class AppShell : Xamarin.Forms.Shell
+    public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
-
     }
 }
